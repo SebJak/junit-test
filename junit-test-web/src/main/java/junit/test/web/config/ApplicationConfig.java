7d4d709cf78ad0147025aaae665eb4c870707c46
Sebastian Jakowski <sebastian.jakowski@beneering.com>
2015-02-17 20:26:49 +0100

    Add Configuration, Hello world Controller, update dependecies

---
+++ junit-test-web/src/main/java/junit/test/web/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package junit.test.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages= {"junit.test.web"})
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ junit-test-web/src/main/java/junit/test/web/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package junit.test.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value="name", required=false, defaultValue="world") String name, Model model){
+		model.addAttribute("name", name);
+		return "helloworld";
+	}
+}
