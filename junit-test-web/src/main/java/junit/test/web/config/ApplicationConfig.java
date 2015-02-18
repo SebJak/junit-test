d66140a937c91f592328acb5c085c0fba6d8f065
Sebastian Jakowski <sebastian.jakowski@beneering.com>
2015-02-18 12:00:53 +0100

    Move config form web.xml to WebInitializer

--- junit-test-web/src/main/java/junit/test/web/config/ApplicationConfig.java
+++ junit-test-web/src/main/java/junit/test/web/config/ApplicationConfig.java
@@ -5,18 +5,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
-@EnableWebMvc
-@ComponentScan(basePackages= {"junit.test.web"})
 @Configuration
+@ComponentScan("junit.test.web")
+@EnableWebMvc
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-    public InternalResourceViewResolver getInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
+	 @Bean  
+	    public UrlBasedViewResolver setupViewResolver() {  
+	        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+	        resolver.setPrefix("/WEB-INF/views/");  
+	        resolver.setSuffix(".jsp");
+	        resolver.setViewClass(JstlView.class);  
+	        return resolver;  
+	    }  
 }
